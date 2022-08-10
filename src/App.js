ddc19ef3ec0ab9b3e15d2adaba9b5e24d6c75da4
teacher_cheater <johnnykayfun@gmail.com>
2022-08-11 01:27:51 +0300

    adв indicators to pictures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch('https://slider.ymatuhin.workers.dev/', {
+      signal: controller.signal,
+    })
+      .then((data) => data.json())
+      .then((response) => setImage(response.images))
+    return () => controller.abort();
+  }, []);
+
+  const data = image.length - 1;
+  const [slideActive, setSlideActive] = useState(0);
+  //const [indicatorsActive, setIndicatorsActive] = useState(true);
+
+
+  useEffect(() => {
+    const time = setInterval(() => {
+      setSlideActive(slideActive === data ? 0 : slideActive + 1);
+    }, 5000);
+    return () => clearInterval(time)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="carousel slide">
+      <div className="carousel-indicators">
+        {image.map((_, index) => (
+          <>
+            <button type="button" data-bs-target />
+            <button key={index} type="button" data-bs-target className={index !== slideActive ? "inactive" : "active"} />
+          </>
+        ))}
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          {image.map((slide, index) => (
+            <div key={index} className={index === slideActive ? "active" : "inactive"}>
+              {<img key={index} className="d-block w-100" src={slide} alt="" />}
+            </div>
+          ))
+          }
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" onClick={() =>
+        setSlideActive(slideActive < 1 ? data : slideActive - 1)
+      }>
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="visually-hidden">Предыдущий</span>
+      </button>
+      <button className="carousel-control-next" type="button" onClick={() =>
+        setSlideActive(slideActive === data ? 0 : slideActive + 1)
+      }>
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="visually-hidden">Следующий</span>
+      </button>
     </div>
   );
 }
 
 export default App;
+
+//https://slider.ymatuhin.workers.dev/ API
